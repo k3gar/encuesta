6bdf18b5ffe6fabf63696c5ee5aed5d31dad9d35
Kevin Garcia <kevinede40@gmail.com>
2024-04-17 00:42:42 -0600

    feat: Styles and Score View Change

--- src/components/Preguntas/index.js
+++ src/components/Preguntas/index.js
@@ -5,6 +5,14 @@ function Encuesta({ datos }) {
   const [respuestas, setRespuestas] = useState(Array.from({ length: datos.secciones.length }, () => ({})));
   const [puntuacionTotalSecciones, setPuntuacionTotalSecciones] = useState([]);
   const [mostrarResultados, setMostrarResultados] = useState(false);
+  const [areaFuncional, setAreaFuncional] = useState('');
+  const [areaFuncionalCodigo, setAreaFuncionalCodigo] = useState('');
+  const [puestoDeTrabajo, setPuestoDeTrabajo] = useState('');
+  const [puestoDeTrabajoCodigo, setPuestoDeTrabajoCodigo] = useState('');
+  const [unidadResponsable, setUnidadResponsable] = useState('');
+  const [unidadResponsableCodigo, setUnidadResponsableCodigo] = useState('');
+  const [fechaAutoevaluacion, setFechaAutoevaluacion] = useState('');
+  const [fechaActualizacion, setFechaActualizacion] = useState('');
   const cantidadSecciones = datos.secciones.length;
 
   const handleChange = (seccionIndex, componenteIndex, preguntaIndex, valor) => {
@@ -64,6 +72,18 @@ function Encuesta({ datos }) {
     setRespuestas(Array.from({ length: cantidadSecciones }, () => ({})));
     setPuntuacionTotalSecciones([]);
     setMostrarResultados(false);
+    setAreaFuncional('');
+    setAreaFuncionalCodigo('');
+    setPuestoDeTrabajo('');
+    setPuestoDeTrabajoCodigo('');
+    setUnidadResponsable('');
+    setUnidadResponsableCodigo('');
+    setFechaAutoevaluacion('');
+    setFechaActualizacion('');
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   };
 
   const calcularPuntuacionGlobal = () => {
@@ -83,44 +103,98 @@ function Encuesta({ datos }) {
           <div className="quiz__welcome__form">
             <div className="quiz__welcome__form--container">
               <div>
-                <label for="area-funcional">Área Funcional</label>
-                <input type="text" id="area-funcional" name="area-funcional" size="30" />
-              </div>
-
-              <div>
-                <label for="area-funcional-codigo">Código</label>
-                <input type="text" id="area-funcional-codigo" name="area-funcional-codigo" size="10" />
-              </div>
-            </div>
-
-            <div className="quiz__welcome__form--container">
-              <div>
-                <label for="puesto-de-trabajo">Puesto de Trabajo:</label>
-                <input type="text" id="puesto-de-trabajo" name="puesto-de-trabajo" size="30" />
+                <label htmlFor="area-funcional">Área Funcional</label>
+                <input
+                  type="text"
+                  id="area-funcional"
+                  name="area-funcional"
+                  size="30"
+                  value={areaFuncional}
+                  onChange={(e) => setAreaFuncional(e.target.value)}
+                />
               </div>
               <div>
-                <label for="puesto-de-trabajo-codigo">Código</label>
-                <input type="text" id="puesto-de-trabajo-codigo" name="puesto-de-trabajo-codigo" size="10" />
-              </div>
-            </div>
-            <div className="quiz__welcome__form--container">
-              <div>
-                <label for="unidad-responsable">Unidad Responsable de Estudio:</label>
-                <input type="text" id="unidad-responsable" name="unidad-responsable" size="30" />
-              </div>
-              <div>
-                <label for="unidad-responsable-codigo">Código</label>
-                <input type="text" id="unidad-responsable-codigo" name="unidad-responsable-codigo" size="10" />
+                <label htmlFor="area-funcional-codigo">Código</label>
+                <input
+                  type="text"
+                  id="area-funcional-codigo"
+                  name="area-funcional-codigo"
+                  size="10"
+                  value={areaFuncionalCodigo}
+                  onChange={(e) => setAreaFuncionalCodigo(e.target.value)}
+                />
               </div>
             </div>
             <div className="quiz__welcome__form--container">
               <div>
-                <label for="fecha-autoevaluación">Fecha Autoevaluación</label>
-                <input type="date" id="fecha-autoevaluación" name="fecha-autoevaluación" size="40" />
+                <label htmlFor="puesto-de-trabajo">Puesto de Trabajo</label>
+                <input
+                  type="text"
+                  id="puesto-de-trabajo"
+                  name="puesto-de-trabajo"
+                  size="30"
+                  value={puestoDeTrabajo}
+                  onChange={(e) => setPuestoDeTrabajo(e.target.value)}
+                />
               </div>
               <div>
-                <label for="fecha-actualizacion">Fecha de Actualización</label>
-                <input type="date" id="fecha-actualizacion" name="fecha-actualizacion" size="40" />
+                <label htmlFor="puesto-de-trabajo-codigo">Código</label>
+                <input
+                  type="text"
+                  id="puesto-de-trabajo-codigo"
+                  name="puesto-de-trabajo-codigo"
+                  size="10"
+                  value={puestoDeTrabajoCodigo}
+                  onChange={(e) => setPuestoDeTrabajoCodigo(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="quiz__welcome__form--container">
+              <div>
+                <label htmlFor="unidad-responsable">Unidad Responsable de Estudio</label>
+                <input
+                  type="text"
+                  id="unidad-responsable"
+                  name="unidad-responsable"
+                  size="30"
+                  value={unidadResponsable}
+                  onChange={(e) => setUnidadResponsable(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="unidad-responsable-codigo">Código</label>
+                <input
+                  type="text"
+                  id="unidad-responsable-codigo"
+                  name="unidad-responsable-codigo"
+                  size="10"
+                  value={unidadResponsableCodigo}
+                  onChange={(e) => setUnidadResponsableCodigo(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="quiz__welcome__form--container">
+              <div>
+                <label htmlFor="fecha-autoevaluacion">Fecha Autoevaluación</label>
+                <input
+                  type="date"
+                  id="fecha-autoevaluacion"
+                  name="fecha-autoevaluacion"
+                  size="40"
+                  value={fechaAutoevaluacion}
+                  onChange={(e) => setFechaAutoevaluacion(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="fecha-actualizacion">Fecha de Actualización</label>
+                <input
+                  type="date"
+                  id="fecha-actualizacion"
+                  name="fecha-actualizacion"
+                  size="40"
+                  value={fechaActualizacion}
+                  onChange={(e) => setFechaActualizacion(e.target.value)}
+                />
               </div>
             </div>
 
@@ -136,15 +210,21 @@ function Encuesta({ datos }) {
           <div className="quiz__welcome__block">
             <h4>Sistema de Calificación</h4>
             <p>
-              <strong>Excelente:</strong> Sobresale por sus óptimas habilidades y competencias en el desarrollo de las diferentes áreas <br />
-              <strong>Muy Bueno:</strong> Responde con eficiencia a las actividades que desarrolla <br />
-              <strong>Bueno:</strong> Posee un desempeño normal <br />
-              <strong>Regular:</strong> Se le dificulta responder de forma eficiente ante la habilidad y competencia presentada <br />
-              <strong>Deficiente:</strong> Un bajo rendimiento en sus capacidades o habilidades <br />
+              <strong>Excelente:</strong> Sobresale por sus óptimas habilidades y competencias en el desarrollo de las diferentes áreas <br /><br />
+              <strong>Muy Bueno:</strong> Responde con eficiencia a las actividades que desarrolla <br /><br />
+              <strong>Bueno:</strong> Posee un desempeño normal <br /><br />
+              <strong>Regular:</strong> Se le dificulta responder de forma eficiente ante la habilidad y competencia presentada <br /><br />
+              <strong>Deficiente:</strong> Un bajo rendimiento en sus capacidades o habilidades <br /><br />
             </p>
           </div>
-          
-          <button className="button-9" onClick={() => setSeccionActualIndex(0)}>Comenzar autoevaluación</button>
+
+          <button className="button-9" onClick={() => {
+            setSeccionActualIndex(0)
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+            })
+            }}>Comenzar autoevaluación</button>
         </div>
       )}
       {seccionActualIndex !== -1 && !mostrarResultados && (
@@ -169,7 +249,6 @@ function Encuesta({ datos }) {
                       </label>
                     ))}
                   </div>
-                  {/* <p>Valor de respuesta: {respuestas[seccionActualIndex]?.[componenteIndex]?.[preguntaIndex]}</p> */}
                 </div>
               ))}
               <hr />
@@ -186,20 +265,41 @@ function Encuesta({ datos }) {
         </div>
       )}
       {mostrarResultados && (
-        <div>
-          <h3>Resultados</h3>
-          <h4>Puntuación Total por Instrumento</h4>
-          <ul>
-            {puntuacionTotalSecciones.map((seccion) => (
-              <li key={seccion.id}>
-                <strong>{seccion.nombre}:</strong> {seccion.puntuacionTotal}
-              </li>
-            ))}
-          </ul>
-          <h4>Puntuación Total</h4>
-          <p>{calcularPuntuacionGlobal()}</p>
+        <section className="quiz__container">
+          <h1>Resultados</h1>
+          <div>
+            <h4>Información Ingresada</h4>
+            <div className="quiz__container--informacion-ingresada">
+              {areaFuncional !== "" && <p> <strong>Área Funcional:</strong>  {areaFuncional}</p>}
+              {areaFuncionalCodigo !== "" && <p> <strong>Código Área Funcional:</strong>  {areaFuncionalCodigo}</p>}
+              {puestoDeTrabajo !== "" && <p> <strong>Puesto de Trabajo:</strong>  {puestoDeTrabajo}</p>}
+              {puestoDeTrabajoCodigo !== "" && <p> <strong>Código Puesto de Trabajo:</strong>  {puestoDeTrabajoCodigo}</p>}
+              {unidadResponsable !== "" && <p> <strong>Unidad Responsable de Estudio:</strong>  {unidadResponsable}</p>}
+              {unidadResponsableCodigo !== "" && <p> <strong>Código Unidad Responsable:</strong>  {unidadResponsableCodigo}</p>}
+              {fechaAutoevaluacion !== "" && <p> <strong>Fecha Autoevaluación:</strong>  {fechaAutoevaluacion}</p>}
+              {fechaActualizacion !== "" && <p> <strong>Fecha de Actualización:</strong>  {fechaActualizacion}</p>}
+            </div>
+          </div>
+          <div>
+
+            <div className="quiz__container--global-score-container">
+              <h2>Puntuación Total</h2>
+              <p className="quiz__container--global-score">{calcularPuntuacionGlobal()}</p>
+            </div>
+            <div className="quiz__container--global-items-container">
+              <h2>Puntuación Total por Instrumento</h2>
+              <ul className="quiz__container--items-container">
+                {puntuacionTotalSecciones.map((seccion) => (
+                  <li className="quiz__container--items-section" key={seccion.id}>
+                    <p className="quiz__container--items-text">{seccion.nombre}</p> <p className="quiz__container--items-text-score">{seccion.puntuacionTotal}</p>
+                  </li>
+                ))}
+              </ul>
           <button className="button-9" onClick={reiniciarEncuesta}>Reiniciar Encuesta</button>
-        </div>
+            </div>
+          </div>
+
+        </section>
       )}
     </div>
   );
